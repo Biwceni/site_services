ae9c6baf4faf051f0afa3803fbc76ddcb9b992ec
Biwceni <fabio.ormeni@hotmail.com>
2022-12-28 19:38:17 -0300

    Site Services Final

--- src/components/api_config/ApiConfig.js
+++ src/components/api_config/ApiConfig.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 // Utilizando a definição da url global do axios
 export default axios.create({
-    baseURL: "https://mysql-services-deploy-render.onrender.com"
+    baseURL: "https://mysql-services-deploy-render.onrender.com",
+    withCredentials: true
 })
